70cf4231e5eb3b52d7d12bb148a1da4da51b83b8
chrcou <christophe@chrcou.fr>
2020-07-14 15:27:24 +0200

    optimisation and bug fix

--- js/helpers.js
+++ js/helpers.js
@@ -1,52 +1,80 @@
 /*global NodeList */
-(function (window) {
-	'use strict';
+// (function(window) {
+'use strict';
+/**
+ * Gets one element by CSS selector
+ * @param {string} selector 
+ * @param {Scope} scope 
+ */
+window.qs = function (selector, scope) {
+    return (scope || document).querySelector(selector);
+};
 
-	// Get element(s) by CSS selector:
-	window.qs = function (selector, scope) {
-		return (scope || document).querySelector(selector);
-	};
-	window.qsa = function (selector, scope) {
-		return (scope || document).querySelectorAll(selector);
-	};
+/**
+ * Gets elements by CSS selector
+ * @param {string} selector 
+ * @param {Scope} scope 
+ */
 
-	// addEventListener wrapper:
-	window.$on = function (target, type, callback, useCapture) {
-		target.addEventListener(type, callback, !!useCapture);
-	};
 
-	// Attach a handler to event for all elements that match the selector,
-	// now or in the future, based on a root element
-	window.$delegate = function (target, selector, type, handler) {
-		function dispatchEvent(event) {
-			var targetElement = event.target;
-			var potentialElements = window.qsa(selector, target);
-			var hasMatch = Array.prototype.indexOf.call(potentialElements, targetElement) >= 0;
+window.qsa = function (selector, scope) {
+    return (scope || document).querySelectorAll(selector);
+};
 
-			if (hasMatch) {
-				handler.call(targetElement, event);
-			}
-		}
 
-		// https://developer.mozilla.org/en-US/docs/Web/Events/blur
-		var useCapture = type === 'blur' || type === 'focus';
+/**
+ * A wrapper for addEventListener
+ * @param {HTMLElement} target the element which will listened
+ * @param {String} type the type of evenement to listen 
+ * @param {Functon} callback a callback function
+ * @param {*} useCapture 	Optional. A Boolean value that specifies whether the event should be executed in the capturing or in the bubbling phase.
+ */
+// addEventListener wrapper:
+window.$on = function (target, type, callback, useCapture) {
+    target.addEventListener(type, callback, !!useCapture);
+};
+/**
+ *  Attach a handler to event for all elements that match the selector,
+ *  now or in the future, based on a root element
+ * @param {HTMLElement} target the element which will listened
+ * @param {string} selector 
+ * @param {String} type the type of evenement to listen 
+ * @param {*} handler The function to call
+ */
 
-		window.$on(target, type, dispatchEvent, useCapture);
-	};
+window.$delegate = function (target, selector, type, handler) {
+    function dispatchEvent(event) {
+        var targetElement = event.target;
+        var potentialElements = window.qsa(selector, target);
+        var hasMatch = Array.prototype.indexOf.call(potentialElements, targetElement) >= 0;
 
-	// Find the element's parent with the given tag name:
-	// $parent(qs('a'), 'div');
-	window.$parent = function (element, tagName) {
-		if (!element.parentNode) {
-			return;
-		}
-		if (element.parentNode.tagName.toLowerCase() === tagName.toLowerCase()) {
-			return element.parentNode;
-		}
-		return window.$parent(element.parentNode, tagName);
-	};
+        if (hasMatch) {
+            handler.call(targetElement, event);
+        }
+    }
 
-	// Allow for looping on nodes by chaining:
-	// qsa('.foo').forEach(function () {})
-	NodeList.prototype.forEach = Array.prototype.forEach;
-})(window);
+    // https://developer.mozilla.org/en-US/docs/Web/Events/blur
+    var useCapture = type === 'blur' || type === 'focus';
+
+    window.$on(target, type, dispatchEvent, useCapture);
+};
+/**
+ * Find the element's parent with the given tag name
+ * @param {HTMLElement} element 
+ * @param {String} tagName 
+ */
+
+window.$parent = function (element, tagName) {
+    if (!element.parentNode) {
+        return;
+    }
+    if (element.parentNode.tagName.toLowerCase() === tagName.toLowerCase()) {
+        return element.parentNode;
+    }
+    return window.$parent(element.parentNode, tagName);
+};
+
+// Allow for looping on nodes by chaining:
+// qsa('.foo').forEach(function () {})
+NodeList.prototype.forEach = Array.prototype.forEach;
+// })(window);
--- js/store.js
+++ js/store.js
@@ -1,157 +1,203 @@
 /*jshint eqeqeq:false */
-(function (window) {
-	'use strict';
+// (function(window) {
+// 'use strict';
 
-	/**
-	 * Creates a new client side storage object and will create an empty
-	 * collection if no collection already exists.
-	 *
-	 * @param {string} name The name of our DB we want to use
-	 * @param {function} callback Our fake DB uses callbacks because in
-	 * real life you probably would be making AJAX calls
-	 */
-	function Store(name, callback) {
-		callback = callback || function () {};
+/**
+ * Creates a new client side storage object and will create an empty
+ * collection if no collection already exists.
+ * @constructor
+ * @param {string} name The name of our DB we want to use
+ * @param {function} callback Our fake DB uses callbacks because in
+ * real life you probably would be making AJAX calls
+ */
+function Store(name, callback) {
+    callback = callback || function () { };
 
-		this._dbName = name;
+    this._dbName = name;
 
-		if (!localStorage[name]) {
-			var data = {
-				todos: []
-			};
+    if (!localStorage[name]) {
+        var data = {
+            todos: []
+        };
 
-			localStorage[name] = JSON.stringify(data);
-		}
+        localStorage[name] = JSON.stringify(data);
+    }
 
-		callback.call(this, JSON.parse(localStorage[name]));
-	}
+    callback.call(this, JSON.parse(localStorage[name]));
+}
 
-	/**
-	 * Finds items based on a query given as a JS object
-	 *
-	 * @param {object} query The query to match against (i.e. {foo: 'bar'})
-	 * @param {function} callback	 The callback to fire when the query has
-	 * completed running
-	 *
-	 * @example
-	 * db.find({foo: 'bar', hello: 'world'}, function (data) {
-	 *	 // data will return any items that have foo: bar and
-	 *	 // hello: world in their properties
-	 * });
-	 */
-	Store.prototype.find = function (query, callback) {
-		if (!callback) {
-			return;
-		}
+/**
+ * Finds items based on a query given as a JS object
+ *
+ * @param {object} query The query to match against (i.e. {foo: 'bar'})
+ * @param {function} callback	 The callback to fire when the query has
+ * completed running
+ *
+ * @example
+ * db.find({foo: 'bar', hello: 'world'}, function (data) {
+ *	 // data will return any items that have foo: bar and
+ *	 // hello: world in their properties
+ * });
+ */
+Store.prototype.find = function (query, callback) {
+    if (!callback) {
+        return;
+    }
 
-		var todos = JSON.parse(localStorage[this._dbName]).todos;
+    var todos = JSON.parse(localStorage[this._dbName]).todos;
 
-		callback.call(this, todos.filter(function (todo) {
-			for (var q in query) {
-				if (query[q] !== todo[q]) {
-					return false;
-				}
-			}
-			return true;
-		}));
-	};
+    callback.call(this, todos.filter(function (todo) {
+        for (var q in query) {
+            if (query[q] !== todo[q]) {
+                return false;
+            }
+        }
+        return true;
+    }));
+};
 
-	/**
-	 * Will retrieve all data from the collection
-	 *
-	 * @param {function} callback The callback to fire upon retrieving data
-	 */
-	Store.prototype.findAll = function (callback) {
-		callback = callback || function () {};
-		callback.call(this, JSON.parse(localStorage[this._dbName]).todos);
-	};
+/**
+ * Will retrieve all data from the collection
+ *
+ * @param {function} callback The callback to fire upon retrieving data
+ */
+Store.prototype.findAll = function (callback) {
+    callback = callback || function () { };
+    callback.call(this, JSON.parse(localStorage[this._dbName]).todos);
+};
 
-	/**
-	 * Will save the given data to the DB. If no item exists it will create a new
-	 * item, otherwise it'll simply update an existing item's properties
-	 *
-	 * @param {object} updateData The data to save back into the DB
-	 * @param {function} callback The callback to fire after saving
-	 * @param {number} id An optional param to enter an ID of an item to update
-	 */
-	Store.prototype.save = function (updateData, callback, id) {
-		var data = JSON.parse(localStorage[this._dbName]);
-		var todos = data.todos;
+/**
+ * Will save the given data to the DB. If no item exists it will create a new
+ * item, otherwise it'll simply update an existing item's properties
+ *
+ * @param {object} updateData The data to save back into the DB
+ * @param {function} callback The callback to fire after saving
+ * @param {number} id An optional param to enter an ID of an item to update
+ */
+Store.prototype.save = function (updateData, callback, id) {
+    var data = JSON.parse(localStorage[this._dbName]);
+    var todos = data.todos;
 
-		callback = callback || function () {};
+    callback = callback || function () { };
 
-		// Generate an ID
-	    var newId = ""; 
-	    var charset = "0123456789";
 
-        for (var i = 0; i < 6; i++) {
-     		newId += charset.charAt(Math.floor(Math.random() * charset.length));
-		}
 
-		// If an ID was actually given, find the item and update each property
-		if (id) {
-			for (var i = 0; i < todos.length; i++) {
-				if (todos[i].id === id) {
-					for (var key in updateData) {
-						todos[i][key] = updateData[key];
-					}
-					break;
-				}
-			}
+    // If an ID was actually given, find the item and update each property
+    if (id) {
+        //TODO : si ok, supprimer les anciens bouts de code
+        //C2 : Deleting "double for" and using find instead.
+        // old code :
+        // for (var i = 0; i < todos.length; i++) {
+        //     if (todos[i].id === id) {
+        //         for (var key in updateData) {
+        //             todos[i][key] = updateData[key];
+        //         }
+        //         break;
+        //     }
+        // }
+        todos.find(
+            (todo) => {
+                if (todo.id === id) {
+                    for (var key in updateData) {
+                        todo[key] = updateData[key];
+                    }
 
-			localStorage[this._dbName] = JSON.stringify(data);
-			callback.call(this, todos);
-		} else {
+                }
+            }
 
-    		// Assign an ID
-			updateData.id = parseInt(newId);
-    
 
-			todos.push(updateData);
-			localStorage[this._dbName] = JSON.stringify(data);
-			callback.call(this, [updateData]);
-		}
-	};
+        );
 
-	/**
-	 * Will remove an item from the Store based on its ID
-	 *
-	 * @param {number} id The ID of the item you want to remove
-	 * @param {function} callback The callback to fire after saving
-	 */
-	Store.prototype.remove = function (id, callback) {
-		var data = JSON.parse(localStorage[this._dbName]);
-		var todos = data.todos;
-		var todoId;
-		
-		for (var i = 0; i < todos.length; i++) {
-			if (todos[i].id == id) {
-				todoId = todos[i].id;
-			}
-		}
+        localStorage[this._dbName] = JSON.stringify(data);
+        callback.call(this, todos);
+    } else {
+        // Generates an ID
+        // The generation fo the id is now in the "else" to avoid creating useless id.
+        //A getNewID function is now used to generating a todo.
+        var newId = this.getNewID(data.todos);
 
-		for (var i = 0; i < todos.length; i++) {
-			if (todos[i].id == todoId) {
-				todos.splice(i, 1);
-			}
-		}
+        // Assign an ID
+        updateData.id = parseInt(newId);
 
-		localStorage[this._dbName] = JSON.stringify(data);
-		callback.call(this, todos);
-	};
 
-	/**
-	 * Will drop all storage and start fresh
-	 *
-	 * @param {function} callback The callback to fire after dropping the data
-	 */
-	Store.prototype.drop = function (callback) {
-		var data = {todos: []};
-		localStorage[this._dbName] = JSON.stringify(data);
-		callback.call(this, data.todos);
-	};
+        todos.push(updateData);
+        localStorage[this._dbName] = JSON.stringify(data);
+        callback.call(this, [updateData]);
+    };
+};
 
-	// Export to window
-	window.app = window.app || {};
-	window.app.Store = Store;
-})(window);
+/**
+ * Will give an ID. This function will also compare a proposition of ID 
+ * with the todos list, if the purposed ID already exists, another ID will 
+ * be emited.
+ *
+ * @param {Array} data An array of already created object. All object must contained an id like this : {...., id: , ...}.
+ */
+Store.prototype.getNewID = function (data) {
+    //C2 replacing this "for" by a simple multiplication
+    // let newId = "";
+    // for (var i = 0; i < 6; i++) {
+    //     newId += charset.charAt(Math.floor(Math.random() * charset.length));
+    // }
+    let newId = Math.floor(Math.random() * 999999);
+    const searchedID = data.find(
+        (t) => {
+            return t.id === newId;
+        }
+    );
+    if (typeof searchedID === "undefined") {
+        return newId;
+
+    } else {
+        return this.getNewID(data);
+    }
+
+
+};
+
+
+/**
+ * Will remove an item from the Store based on its ID
+ *
+ * @param {number} id The ID of the item you want to remove
+ * @param {function} callback The callback to fire after saving
+ */
+Store.prototype.remove = function (id, callback) {
+    var data = JSON.parse(localStorage[this._dbName]);
+    var todos = data.todos;
+    // var todoId;
+    //TODO : si ok, supprimer ancien code
+    //ligne 155 : simplification de la boucle for, si l'id est trouvé, suppresion.
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == id) {
+            // todoId = todos[i].id;
+            todos.splice(i, 1);
+        }
+        break;
+    }
+
+    // for (var i = 0; i < todos.length; i++) {
+    //     if (todos[i].id == todoId) {
+    //         todos.splice(i, 1);
+    //     }
+    // }
+
+    localStorage[this._dbName] = JSON.stringify(data);
+    callback.call(this, todos);
+};
+
+/**
+ * Will drop all storage and start fresh
+ *
+ * @param {function} callback The callback to fire after dropping the data
+ */
+Store.prototype.drop = function (callback) {
+    var data = { todos: [] };
+    localStorage[this._dbName] = JSON.stringify(data);
+    callback.call(this, data.todos);
+};
+
+// Export to window
+window.app = window.app || {};
+window.app.Store = Store;
+// }) (window);
